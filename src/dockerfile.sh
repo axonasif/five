7d69de49813d1cce404d4e6998dee2d69d0c8fbb
AXON <axonasif@gmail.com>
2022-01-16 23:00:03 +0600

    Testing stage reached, attempt #3

--- src/dockerfile.sh
+++ src/dockerfile.sh
@@ -13,8 +13,7 @@ COPY install-packages /usr/bin/ # What's below is not included in the result doc
 # _pkg_list comes from src/subcommand/compile/mod.sh
 RUN <<EOF
 install-packages ${_pkg_list[@]} \
-	&& useradd -l -u 33333 -G sudo -md /home/gitpod -s /bin/bash -p gitpod gitpod \
-    	&& sed -i.bak -e 's/%sudo\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers \
+	&& for _try in sudo wheel; do if useradd -l -u 33333 -G $_try -md /home/gitpod -s /bin/bash -p gitpod gitpod; then sed -i.bak -e "s/%$_try\s\+ALL=(ALL\(:ALL\)\?)\s\+ALL/%$_try ALL=NOPASSWD:ALL/g" /etc/sudoers && break; done \
 	&& printf '%s\n' "PS1='\[]0;\u \w\]\[[01;32m\]\u\[[00m\] \[[01;34m\]\w\[[00m\] \$ '" \
 		'source "\$HOME/.bashrc.d"/*' | sudo -u gitpod tee -a "/home/gitpod/.bashrc" \
 	&& sudo -u gitpod mkdir -m0755 -p /home/gitpod/.bashrc.d
